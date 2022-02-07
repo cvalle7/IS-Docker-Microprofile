1e9ad7feb3b88ab1abba9f563e2bd460b76adfac
cvalle7 <79222562+cvalle7@users.noreply.github.com>
2022-02-07 19:19:52 +0100

    Update payara.sh

--- apps/BackendF1/payara.sh
+++ apps/BackendF1/payara.sh
@@ -1,7 +1,7 @@
 export DATABASE_DRIVER="oracle.jdbc.pool.OracleDataSource"
 export DATABASE_TYPE="oracle"
 export DATABASE_URL="thin:@localhost:1521:xe"
-export DATABASE_USER="VAIE"
-export DATABASE_PSW="bd123"
+export DATABASE_USER="yourUser"
+export DATABASE_PSW="yourPsswrd"
 
 java -jar /Payara/micro/payara-micro-5.2020.4.jar  --port 88 --deploy ./target/BackendF1-1.0.war --contextroot /back   
